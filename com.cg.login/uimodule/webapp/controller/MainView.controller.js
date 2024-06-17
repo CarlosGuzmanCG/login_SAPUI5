5e7736500a4354773e260fe6c04f520d6aded7cd
CarlosGuzCG <166045537+CarlosGuzCG@users.noreply.github.com>
2024-06-16 22:40:19 -0600

    first view css style

--- com.cg.login/uimodule/webapp/controller/MainView.controller.js
+++ com.cg.login/uimodule/webapp/controller/MainView.controller.js
@@ -4,12 +4,13 @@ sap.ui.define([
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
+
     function (Controller) {
         "use strict";
 
         return Controller.extend("com.cg.login.controller.MainView", {
             onInit: function () {
-
+                sap.ui.getCore().applyTheme("sap_fiori_3_dark");
             }
         });
     });
