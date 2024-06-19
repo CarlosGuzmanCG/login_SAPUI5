895d439b4dda2e80977cd10341b859be25f00166
CarlosGuzCG <166045537+CarlosGuzCG@users.noreply.github.com>
2024-06-19 00:46:31 -0600

    creation of user using up page

--- com.cg.login/uimodule/webapp/controller/SignUp.controller.js
+++ com.cg.login/uimodule/webapp/controller/SignUp.controller.js
@@ -1,4 +1,7 @@
-sap.ui.define(["com/cg/login/controller/BaseController"], function (Controller) {
+sap.ui.define([
+    "com/cg/login/controller/BaseController",
+    "sap/m/MessageBox"
+], function (Controller,MessageBox) {
     "use strict";
 
     return Controller.extend("com.cg.login.controller.SignUp", {
@@ -8,10 +11,10 @@ sap.ui.define(["com/cg/login/controller/BaseController"], function (Controller) 
             const oUserModel = this.getOwnerComponent().getModel("userDetails"); //get model
 
             const oDetails = { //json
-                name: "carlos",
-                email: "carlos@gmail.com",
-                password: "test01",
-                cnfPassword: "test01"
+                name: "",
+                email: "",
+                password: "",
+                cnfPassword: ""
             }
 
             oUserModel.setProperty("/signUpDetails", oDetails); //set model
@@ -19,7 +22,19 @@ sap.ui.define(["com/cg/login/controller/BaseController"], function (Controller) 
         },
 
         onPressSignIn:function(oEvent){
-            debugger;
+           const oUserModel = this.getOwnerComponent().getModel("userDetails"); //get model
+           const oDetails = oUserModel.getProperty("/signUpDetails"); //get json
+
+           //logic to create an account for user
+           const oFirebaseModel = this.getOwnerComponent().getModel("firebase").getData(); // get data from model firebase
+           oFirebaseModel.fireauth.createUserWithEmailAndPassword(oDetails.email, oDetails.password)
+            .then((userCredential) => {
+                MessageBox.success("Account created successfully!");
+          })
+          .catch((error) => {
+                MessageBox.error(error.message);
+          });
+           
         },
 
         navToSignIn: function(){
