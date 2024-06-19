c59705bf3b36e6dba2dbf0a18ff3b714a1530d8c
CarlosGuzCG <166045537+CarlosGuzCG@users.noreply.github.com>
2024-06-18 19:37:08 -0600

    C:/Program Files/Git/model

--- com.cg.login/uimodule/webapp/controller/SignUp.controller.js
+++ com.cg.login/uimodule/webapp/controller/SignUp.controller.js
@@ -2,8 +2,24 @@ sap.ui.define(["com/cg/login/controller/BaseController"], function (Controller) 
     "use strict";
 
     return Controller.extend("com.cg.login.controller.SignUp", {
+
         onInit(){
             sap.ui.getCore().applyTheme("sap_fiori_3_dark");
+            const oUserModel = this.getOwnerComponent().getModel("userDetails"); //get model
+
+            const oDetails = { //json
+                name: "carlos",
+                email: "carlos@gmail.com",
+                password: "test01",
+                cnfPassword: "test01"
+            }
+
+            oUserModel.setProperty("/signUpDetails", oDetails); //set model
+
+        },
+
+        onPressSignIn:function(oEvent){
+            debugger;
         },
 
         navToSignIn: function(){
